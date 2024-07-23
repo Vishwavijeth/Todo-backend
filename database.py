5db90893bb7c39e6f923fd2fed11fedc4d29a026
Vichu <93145890+Vishwavijeth@users.noreply.github.com>
2024-07-23 22:15:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgresql@localhost/TodoDatabase'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todoApp.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
